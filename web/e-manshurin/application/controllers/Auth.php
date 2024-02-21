55c8e0c338946809080fed3dd70a1e07579fc27e
Ari Muhammad Rizal <arimrizal94@gmail.com>
2024-02-21 22:18:53 +0700

    feature: register

--- web/e-manshurin/application/controllers/Auth.php
+++ web/e-manshurin/application/controllers/Auth.php
@@ -5,15 +5,44 @@ class Auth extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'E-Manshurin | Login';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
 
     public function register()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/register');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has been registered'
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[3]|matches[repassword]', [
+            'matches' => 'The Password field does not match',
+            'min_lenght' => 'The Password too short'
+        ]);
+        $this->form_validation->set_rules('repassword', 'Repeat Password', 'matches[password]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'E-Manshurin | Register';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash(htmlspecialchars($this->input->post('password', true)), PASSWORD_DEFAULT),
+                'role_id' => '2',
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulation! your account has been created. Please Login</div>');
+            redirect('auth');
+        }
     }
 }
